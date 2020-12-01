b87b2f03373115b1e0e28ef8ba801703f808202e
Fame <kruassan858@gmail.com>
2020-12-01 19:54:44 +0300

    3 les

---
+++ index2.php
@@ -0,0 +1,24 @@
+<?php
+$arr = [1. 2. 3];
+$list1 = [1. 2. 3];
+$list = [
+'red' => 1,
+'blue'=> 2,
+'green'=> 3,
+];
+$end = false;
+while (!end)
+	$i=0;
+$current = 1;
+{
+if (empty($current))
+{
+$end = true;
+}
+else
+{
+	$current = array_pop($list); 
+	var_export($current);
+} 	
+}
+var_export(array_merge($list, $arr));
